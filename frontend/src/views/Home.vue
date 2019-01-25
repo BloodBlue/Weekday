c95d5d55fd315d20ec4868495b85e783ef15b046
BloodBlue <704829754@qq.com>
2019-01-25 16:27:17 +0800

    首页修改

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div>
     <el-button @click="getStatus()">查看最新状态</el-button>
     <div v-for="item of statuslist" :key="item.index">
-      <p>{{item.title}}{{item.date}}</p>
+      <p>{{item.title}} {{item.date}}</p>
       <el-progress :text-inside="true" :stroke-width="18" :percentage="item.percentage" color="item.color"></el-progress>
     </div>
   </div>
@@ -24,7 +24,7 @@ export default {
         {index: 4, title: '', date: '', percentage: 0, color: 'rgba(142, 113, 199, 0.7)'},
         {index: 5, title: '', date: '', percentage: 0, color: 'rgba(142, 113, 199, 0.7)'},
         {index: 6, title: '', date: '', percentage: 0, color: 'rgba(142, 113, 199, 0.7)'},
-        {index: 7, title: '', date: '', percentage: 0, color: 'rgba(142, 113, 199, 0.7)'}
+        {index: 7, title: '调查问卷', date: '', percentage: 0, color: 'rgba(142, 113, 199, 0.7)'}
       ]
     }
   },
@@ -44,31 +44,8 @@ export default {
         .then(val => {
           this.day = val.data.data.sevendays
           this.date = val.data.data.date
-          console.log('dict:', this.date)
+          console.log('day:', this.day)
         })
-      // Object.keys(this.day).forEach(element => {
-      //   if (element === '0') {
-      //     this.statuslist[1].percentage = 100
-      //   }
-      //   if (element === '1') {
-      //     this.statuslist[2].percentage = 100
-      //   }
-      //   if (element === '2') {
-      //     this.statuslist[3].percentage = 100
-      //   }
-      //   if (element === '3') {
-      //     this.statuslist[4].percentage = 100
-      //   }
-      //   if (element === '4') {
-      //     this.statuslist[5].percentage = 100
-      //   }
-      //   if (element === '5') {
-      //     this.statuslist[6].percentage = 100
-      //   }
-      //   if (element === '6') {
-      //     this.statuslist[7].percentage = 100
-      //   }
-      // })
     },
     // 获取题目对应title
     get_title (num) {
@@ -80,7 +57,16 @@ export default {
       var index = 0
       for (var key in this.date) {
         this.statuslist[index].date = key
-        this.statuslist[index].title = get_title(this.date[key])
+        this.statuslist[index].title = this.get_title(this.date[key])
+        index = index + 1
+      }
+      index = 0
+      for (var key1 in this.day) {
+        for (var key2 in this.statuslist) {
+          if (this.statuslist[key2].title === this.get_title(key1)) {
+            this.statuslist[key2].percentage = 100
+          }
+        }
       }
     }
   }
